b739200173b52fd6c5419c9901115c3cb3fbbfca
Erin Laidley <eclaidley@gmail.com>
2018-05-05 12:48:57 -0400

    add email verification

---
+++ app.js
@@ -0,0 +1,24 @@
+var emailInput = document.querySelector("#email");
+var signUp = document.querySelector("#sign-up");
+var alertMsg = document.querySelector("#alert-msg");
+var userInterest = document.querySelector(".selected-interest");
+
+// after clicking "Sign Up" button, ensure user's email is valid
+signUp.addEventListener("click", function(e){
+	e.preventDefault();
+	validEmail();
+});
+
+// checks if email that was inputted by the user matches the standard email format
+function validEmail(){
+	var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	if(emailInput.value.match(emailFormat)){
+		alertMsg.innerHTML = "";
+		// change "Sign Up" button text to "Submitting..."
+		signUp.innerHTML = "Submitting...";
+	}
+	else {
+		alertMsg.innerHTML = "Please enter a valid email address";
+		alertMsg.style.color = "#C23628";
+	}
+}
