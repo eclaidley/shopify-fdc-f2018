d6209513b1003db43f4d4eb50ba8c39b94cd8348
Erin Laidley <eclaidley@gmail.com>
2018-05-07 10:01:03 -0400

    add successful submission screen

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var emailInput = document.querySelector("#email");
 var signUp = document.querySelector("#sign-up");
 var alertMsg = document.querySelector("#alert-msg");
 var userInterest = document.querySelector(".selected-interest");
+var submissionHeader = document.querySelector("#submission-header");
+var submissionForm = document.querySelector("#submission-form");
 
 // after clicking "Sign Up" button, ensure user's email is valid
 signUp.addEventListener("click", function(e){
+	// prevents page reload upon submission of form
 	e.preventDefault();
 	validEmail();
 });
@@ -14,13 +17,28 @@ function validEmail(){
 	var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 	if(emailInput.value.match(emailFormat)){
 		alertMsg.innerHTML = "";
+		// log value of each field to browser console
 		console.log(emailInput.value);
 		console.log(userInterest.value);
-		// change "Sign Up" button text to "Submitting..."
-		signUp.innerHTML = "Submitting...";
+		submittingButton();
+		// keeps "Submitting..." button up for 2 seconds
+		setTimeout(function() {
+			successfulSubmission();
+		}, 2000);
 	}
 	else {
 		alertMsg.innerHTML = "Please enter a valid email address";
 		alertMsg.style.color = "#C23628";
 	}
+}
+
+function submittingButton(){
+	// change "Sign Up" button text to "Submitting..."
+	signUp.innerHTML = "Submitting...";
+}
+
+function successfulSubmission(){
+	submissionHeader.innerHTML = "Thanks for subscribing";
+	submissionForm.innerHTML = "You'll start receiving free tips and resources soon.";
+	submissionForm.style.color = "white";
 }
